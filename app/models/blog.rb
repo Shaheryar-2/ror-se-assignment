cc612330cd0fc4a1e8e873ffe4e926e78345eeaa
Shaheryar-2 <sharymirza457@gmail.com>
2025-01-19 17:55:06 +0500

    Added validations for blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,7 @@
 class Blog < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :body, presence: true
+  validates :user_id, presence: true
 end
